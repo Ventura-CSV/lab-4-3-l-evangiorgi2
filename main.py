ccb25ab172ab3550814c703c4ca8b0a68929014d
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 13:54:07 -0700

    For Loop Complete

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 def main():
+    
     total = 0
-
     numbers = [0] * 5
     for i in range(len(numbers)):
         numbers[i] = int(input('Enter a value: '))
-
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-
-    # total = sum(numbers)
+        total = sum(numbers)
+    print(numbers)
     print(total)
+    
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
     return total, numbers
 
 
